c1fbcf0fce7520e4a52ca082dbbe519e46ee6ec0
VadimZverev <39337335+VadimZverev@users.noreply.github.com>
2018-08-29 10:55:48 +0400

    Add project files.

---
+++ Views/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace Views.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewData["Message"] = "Привет Мир";
+            ViewData["Time"] = DateTime.Now.ToShortTimeString();
+            return View("DebugData");
+        }
+
+        public ActionResult List()
+        {
+            return View();
+        }
+    }
+}
