bd5673530f993cefdca66d8d0d0f1707007aab7a
VadimZverev <39337335+VadimZverev@users.noreply.github.com>
2018-08-29 11:16:24 +0400

    Создаем реализацию IViewEngine

---
+++ Views/Infrastructure/DebugDataViewEngine.cs
@@ -0,0 +1,29 @@
+﻿using System.Web.Mvc;
+
+namespace Views.Infrastructure
+{
+    public class DebugDataViewEngine : IViewEngine
+    {
+        public ViewEngineResult FindPartialView(ControllerContext controllerContext, string partialViewName, bool useCache)
+        {
+            return new ViewEngineResult(new string[] { "Нет представления (Debug Data Engine)" });
+        }
+
+        public ViewEngineResult FindView(ControllerContext controllerContext, string viewName, string masterName, bool useCache)
+        {
+            if (viewName == "DebugData")
+            {
+                return new ViewEngineResult(new DebugDataView(), this);
+            }
+            else
+            {
+                return new ViewEngineResult(new string[] { "Нет представления (Debug Data Engine)" });
+            }
+        }
+
+        public void ReleaseView(ControllerContext controllerContext, IView view)
+        {
+            // ничего не делает
+        }
+    }
+}
